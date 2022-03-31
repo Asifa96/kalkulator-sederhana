5ace41e998ee1262b76465c0231bcc1e43f65b43
Asifa <101761209+Asifa96@users.noreply.github.com>
2022-04-01 00:58:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+      
+        function penjumlahan() {
+            var no1 = document.form.bil1.value;
+            var no2 = document.form.bil2.value;
+            var a = parseInt(no1)+parseInt(no2);
+            alert("Hasil Penjumlahan = "+a);
+        }
+
+        function pengurangan(){
+            var no1 = document.form.bil1.value;
+            var no2 = document.form.bil2.value;
+            var b= no1-no2;
+            alert("Hasil Pengurangan = "+b);
+        }
+
+        function perkalian(){
+            var no1 = document.form.bil1.value;
+            var no2 = document.form.bil2.value;
+            var c= no1*no2;
+            alert("Hasil Perkalian = "+c);
+        }
+
+        function pembagian(){
+            var no1 = document.form.bil1.value;
+            var no2 = document.form.bil2.value;
+            var d= no1/no2;
+            alert("Hasil Pembagian = "+d);
+        }
+        
